1b47088e122e218585e3c76499423923d485b900
maurozem <maurozem@brq.com>
2021-10-22 15:21:25 -0300

    Implementação do ToastSnakeFragment

--- app/src/main/java/br/com/programadordeelite/gdc/codelab/core/toast/ToastSnakeFragment.kt
+++ app/src/main/java/br/com/programadordeelite/gdc/codelab/core/toast/ToastSnakeFragment.kt
@@ -1,11 +1,10 @@
 package br.com.programadordeelite.gdc.codelab.core.toast
 
-import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import br.com.programadordeelite.gdc.R
+import br.com.programadordeelite.gdc.codelab.util.snake
 import br.com.programadordeelite.gdc.codelab.util.toast
 import br.com.programadordeelite.gdc.databinding.FragmentToastSnakeBinding
 import com.google.android.material.snackbar.Snackbar
@@ -17,9 +16,24 @@ class ToastSnakeFragment : Fragment(R.layout.fragment_toast_snake) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // +---------------------------------------------------------------------------------+
-        // | ESCREVA SEU CÓDIGO ACOMPANHANDO A AULA NO YOUTUBE                               |
-        // | JÁ APROVEITA E SEGUE O CANAL >> LINK PARA AULA: https://youtu.be/9vfXfq1RfTo    |
-        // +---------------------------------------------------------------------------------+
+        binding = FragmentToastSnakeBinding.bind(view)
+        setListners()
+    }
+
+    private fun setListners() {
+        binding.toast.setOnClickListener {
+            toast("elemetar meu caro")
+        }
+        binding.snake.setOnClickListener {
+            snake(binding.root, "snake é cobra")
+        }
+        binding.snakeAction.setOnClickListener {
+            Snackbar
+                .make(binding.root, "atenção: snake é cobra!!", Snackbar.LENGTH_LONG)
+                .setAction("okay") {
+                    toast("parabéns")
+                }
+                .show()
+        }
     }
 }
